422c192e609dea09d96e69a14847242d51879197
bfriasaraya <47584226+bfriasaraya@users.noreply.github.com>
2025-04-10 10:15:03 -0700

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,4 @@
+let quote = document.getElementById("quote");
+quote.onclick = function(){
+    alert('"Manga endings might always be the same. However... when it comes to life, neither you or I are mere readers. We are the writers. We can change the ending." - Sakata Gintoki (Gintama)');
+}
